71228fde79bb2550b23145c436ac4c3f2891797b
Kartik0916 <kartikgupta0916@gmail.com>
2024-09-12 14:03:52 +0530

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -180,7 +180,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorised request")
     }
 
@@ -229,5 +229,5 @@ export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken 
 }
