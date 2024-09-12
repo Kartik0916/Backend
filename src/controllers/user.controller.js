fd60cbddc1b469ba52857978f1729aa6fcb8d7b0
Kartik0916 <kartikgupta0916@gmail.com>
2024-09-12 23:55:15 +0530

    fix removes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -157,7 +157,7 @@ const logoutUser = asyncHandler(async(req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1
+                refreshToken: 1 // this removes the field from do
             }
         },
         {
